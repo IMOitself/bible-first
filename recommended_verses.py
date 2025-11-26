bc3c53d6a3f9d1f469d0f76947f18e38a6707f8b
IMOitself <180146096+IMOitself@users.noreply.github.com>
2025-11-26 15:31:38 +0800

    add missing recommended_verses.py

---
+++ recommended_verses.py
@@ -0,0 +1,30 @@
+
+verses = [
+    # 1 Timothy 3:15
+    # 53th book, Chapter 3 (2), Verse 15 (14)
+    [53, 2, 14],
+
+    # Proverbs 22:6
+    # 19th book, Chapter 22 (21), Verse 6 (5)
+    [19, 21, 5],
+
+    # Hebrews 10:25
+    # 57th book, Chapter 9 (10), Verse 24 (23)
+    [57, 9, 24],
+
+    # Galatians 6:2
+    # 47th book, Chapter 5 (6), Verse 1 (0)
+    [47, 6, 1],
+
+    # Ephesians 6:1
+    # 48th book, Chapter 5 (6), Verse 1 (0)
+    [48, 5, 0],
+
+    # 2 Timothy 3:15
+    # 54th book, Chapter 2 (3), Verse 15 (14)
+    [54, 2, 14],
+
+    # Matthew 6:27
+    # 39th book, Chapter 5 (6), Verse 26 (25)
+    [39, 5, 26],
+]
